5f11820b65aa44036bf1b4da02c269c2901e7efa
Wojciech Zajkowski <traumatictiltexperience@gmail.com>
2025-12-02 21:51:39 -0500

    removing dd as Esc; telescope search tweaks

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,19 +5,20 @@
 --  See `:help hlsearch`
 
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+-- up/down by line across wrapped lines
+vim.keymap.set('n', 'j', 'gj')
+vim.keymap.set('n', 'k', 'gk')
+vim.keymap.set('n', '<Down>', 'gj')
+vim.keymap.set('n', '<Up>', 'gk')
 
 -- this is necessary because to reduce the lag when pressing esc. The lag is caused by the command above: since esc is also used for removing the search highlighting
 vim.keymap.set('i', 'hh', '<Esc>')
-vim.keymap.set('i', 'dd', '<Esc>')
+-- vim.keymap.set('i', 'dd', '<Esc>')
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
--- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
--- is not what someone will guess without a bit more experience.
---
--- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
--- or just use <C-\><C-n> to exit terminal mode
+-- for people to discover. Otherwise, you normally need to press <C-\''\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 -- TIP: Disable arrow keys in normal mode
