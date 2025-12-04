29bce8dd9e5e7ef357445643ee0acc499a013956
Wojciech Zajkowski <traumatictiltexperience@gmail.com>
2025-12-03 23:15:41 -0500

    gitsigns config

--- lua/kickstart/plugins/gitsigns.lua
+++ lua/kickstart/plugins/gitsigns.lua
@@ -6,6 +6,15 @@ return {
   {
     'lewis6991/gitsigns.nvim',
     opts = {
+      signs = {
+        add = { text = '┃' },
+        change = { text = '┋' },
+        delete = { text = '' },
+        topdelete = { text = '' },
+        changedelete = { text = '┃' },
+      },
+      attach_to_untracked = true,
+      current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
       on_attach = function(bufnr)
         local gitsigns = require 'gitsigns'
 
