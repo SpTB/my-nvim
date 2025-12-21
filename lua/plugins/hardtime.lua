b4d97fc6fc0324d717a88887c98fec3c84b327a2
Wojciech Zajkowski <traumatictiltexperience@gmail.com>
2025-12-21 04:08:39 -0500

    semi-fix to blink-cmp working with hardtime plugin (works for c-n, c-p & c-y)

--- lua/plugins/hardtime.lua
+++ lua/plugins/hardtime.lua
@@ -2,5 +2,18 @@ return {
   'm4xshen/hardtime.nvim',
   lazy = false,
   dependencies = { 'MunifTanjim/nui.nvim' },
-  opts = {},
+  opts = {
+    excluded_keys = {
+      -- Add other keys you want to exclude here
+      ['<C-y>'] = true, -- Exclude Ctrl+y from hardtime's restrictions
+      ['<C-n>'] = true, -- Exclude Ctrl+n if used for closing completion
+      ['<C-p>'] = true, -- Exclude Ctrl+p if used for prev completion
+      ['<C-e>'] = true, -- Exclude Ctrl+e if used for cancel completion
+      ['<C-b>'] = true, -- Exclude Ctrl+b if used for scrolling docs up
+      ['<C-f>'] = true, -- Exclude Ctrl+f if used for scrolling docs down
+      -- Depending on your blink.cmp setup, you might need to exclude others like <CR> or <Tab>
+      -- ['<CR>'] = true,
+      -- ['<Tab>'] = true,
+    },
+  },
 }
